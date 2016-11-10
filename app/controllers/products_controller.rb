07b5f17cf1cc8c9c2a08da8c52cb922ccb4c692c
maitrey-shroff <maitrey89@me.com>
2016-11-09 20:40:40 -0800

    added products

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+
+  def home
+    @products = Product.all
+
+  end
+
+  def first_product
+
+  end
+
+  def second_product
+
+  end
+
+  def third_product
+
+  end
+
+  def fourth_product
+
+  end
+
+  def fifth_product
+
+  end
+
+
+end
