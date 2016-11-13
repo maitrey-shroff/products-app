0da4b8afdd0a2bf858ad78c502f2765fb5a4ed85
maitrey-shroff <maitrey89@me.com>
2016-11-13 15:31:00 -0800

    formatting updates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,27 +2,6 @@ class ProductsController < ApplicationController
 
   def home
     @products = Product.all
-
-  end
-
-  def first_product
-
-  end
-
-  def second_product
-
-  end
-
-  def third_product
-
-  end
-
-  def fourth_product
-
-  end
-
-  def fifth_product
-
   end
 
 
