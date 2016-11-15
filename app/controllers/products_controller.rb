d2d45b0bb8c777797b088b2a46a9767b67d1d4a5
maitrey-shroff <maitrey89@me.com>
2016-11-14 19:35:41 -0800

    added show page to show each product and link to show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,5 +4,12 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to:"products#home"
 
+  get "/products", to:"products#index"
+
+  get "/products/:id", to:"products#show"
+
 
 end
