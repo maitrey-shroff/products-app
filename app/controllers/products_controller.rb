37fb1994c6f71ad20343b7e4054ae99baa47e059
maitrey-shroff <maitrey89@me.com>
2016-11-16 19:29:54 -0800

    added navbar and redirects

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,6 +15,10 @@ class ProductsController < ApplicationController
   def save_product
     @product = Product.new({product_name: params[:product_name], description: params[:description], image: params[:image], price: params[:price]})
     @product.save
+
+    flash[:success] = "Product saved!"
+
+    redirect_to "/products"
   end
 
   def edit
@@ -25,13 +29,19 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params[:id])
     product.assign_attributes({product_name: params[:product_name], description: params[:description], image: params[:image], price: params[:price]})
     product.save
+
+    flash[:info] = "Product updated!"
+
+    redirect_to "/products/#{product.id}"
   end
 
   def destroy
     product = Product.find_by(id: params[:id])
     product.destroy
 
-    
+    flash[:danger] = "Product deleted!"
+
+    redirect_to "/products"
   end
 
 end
